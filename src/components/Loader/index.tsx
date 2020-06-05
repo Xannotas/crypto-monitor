352399c761e090e124d2cfa98bc98533692878c0
xannotas <xannotas@gmail.com>
2020-06-05 18:53:49 +0700

    edited loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -3,11 +3,12 @@ import classNames from 'classnames'
 
 type TProps = {
   className?: string
+  center?: boolean
 }
 
-const Loader: React.FC<TProps> = ({ className }) => {
+const Loader: React.FC<TProps> = ({ className, center = true }) => {
   return (
-    <div className={classNames("d-flex justify-content-center", className)}>
+    <div className={classNames({"d-flex justify-content-center" : center}, className)}>
       <div className="spinner-border" role="status">
         <span className="sr-only">Loading...</span>
       </div>
