2af56ba8e447343a9b89eb9c6d9c820236545572
xannotas <xannotas@gmail.com>
2020-06-07 23:24:52 +0700

    loader in converter

--- src/containers/CurrencyConverter/index.tsx
+++ src/containers/CurrencyConverter/index.tsx
@@ -1,38 +1,48 @@
 import React from 'react'
-import CurrencyConverterForm from '../../components/CurrencyConverterForm'
+import { connect } from 'react-redux'
 
 import './currencyConverter.scss'
 import { TCoinCode } from '../../types'
-import { connect } from 'react-redux'
 import { TRootState } from '../../store'
-import { priceSelector } from '../../store/currencyConverter/selectors'
+import { priceSelector, isFetchingSelector } from '../../store/currencyConverter/selectors'
 import { getPrice } from '../../store/currencyConverter/actions'
+
+import Loader from '../../components/Loader'
+import CurrencyConverterForm from '../../components/CurrencyConverterForm'
 
 type TMapState = {
   // currencyCode: TCoinCode,
   // targetCurrencyCode: TCoinCode,
   price: number,
+  isFetching: boolean
 }
 
 type TMapDispatch = {
   getPrice: (currency: TCoinCode, targetCurrency: TCoinCode) => void
 }
 
-const CurrencyConverter: React.FC<TMapState & TMapDispatch> = ({ price, getPrice }) => {
+type TProps = TMapState & TMapDispatch
+
+const CurrencyConverter: React.FC<TProps> = ({ price, getPrice, isFetching }) => {
   const onCurrencyChange = (currency: TCoinCode, targetCurrency: TCoinCode) => {
     getPrice(currency, targetCurrency)
   }
 
   return (
     <div className='currency-converter'>
-      <h5>Currency converter</h5>
+      <h5>Currency converter
+        {isFetching &&
+          <span className="spinner-border spinner-border-sm ml-2" role="status" aria-hidden="true"></span>
+        }
+      </h5>
       <CurrencyConverterForm price={price} onCurrencyChange={onCurrencyChange} />
     </div>
   )
 }
 
 const mapState = (state: TRootState): TMapState => ({
-  price: priceSelector(state)
+  price: priceSelector(state),
+  isFetching: isFetchingSelector(state),
 })
 
 export default connect<TMapState, TMapDispatch, {}, TRootState>(mapState, { getPrice })(CurrencyConverter)
--- src/store/currencyConverter/selectors/index.ts
+++ src/store/currencyConverter/selectors/index.ts
@@ -1,3 +1,4 @@
 import { TRootState } from '../..'
 
 export const priceSelector = (state: TRootState) => state.currencyConverter.price
+export const isFetchingSelector = (state: TRootState) => state.currencyConverter.isFetching
