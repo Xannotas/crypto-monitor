89250ff0748a61acb4c0d8ccee28ba40f189bce0
xannotas <xannotas@gmail.com>
2020-06-08 00:52:16 +0700

    Updates

---
+++ precache-manifest.98966721702e49c45632b1507cddbb59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69eaf481353aed16786ef15a28d1bb2b",
+    "url": "/crypto-monitor/index.html"
+  },
+  {
+    "revision": "99c42e2c6b1d4dda2601",
+    "url": "/crypto-monitor/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "42a1d366e0dde671bdbd",
+    "url": "/crypto-monitor/static/css/main.ef7826e7.chunk.css"
+  },
+  {
+    "revision": "99c42e2c6b1d4dda2601",
+    "url": "/crypto-monitor/static/js/2.ebf01b78.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/crypto-monitor/static/js/2.ebf01b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a1d366e0dde671bdbd",
+    "url": "/crypto-monitor/static/js/main.264a0803.chunk.js"
+  },
+  {
+    "revision": "8be2beec4f93e07f5412",
+    "url": "/crypto-monitor/static/js/runtime-main.8c0c6b53.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crypto-monitor/precache-manifest.6428710c5a79152eea83c9c899a53707.js"
+  "/crypto-monitor/precache-manifest.98966721702e49c45632b1507cddbb59.js"
 );
 
 self.addEventListener('message', (event) => {
