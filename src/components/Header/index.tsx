fe728b23b98c370972435c4330fab45bc34f22ee
xannotas <xannotas@gmail.com>
2020-06-09 19:30:35 +0700

    edits

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,25 +2,25 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import './header.scss'
+import logoPng from './../../assets/header-logo.png'
 
 type TProps = {
 
 }
 
 const Header: React.FC<TProps> = () => {
-
-  const logoUrl = 'https://uploads-ssl.webflow.com/5d5f02be87f780005e708e1f/5db01a351c624d3ecf624e6d_reactlogo.jpg'
-
   return (
     <div className='header'>
       <div className="container">
-        <div className="header-logo">
-          <Link to='/'>
-            <img src={logoUrl} alt="logo" />
-          </Link>
+        <div className="row">
+          <div className="col-md-3">
+            <Link to='/home' className='header-logo'>
+              <img src={logoPng} alt=""/>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
