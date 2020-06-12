225d9b9ad070929a4c0ef225d867541c374a3795
xannotas <xannotas@gmail.com>
2020-06-12 19:59:34 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crypto-monitor/precache-manifest.06271d1f46d5f48e7c979ffc5e3c58ea.js"
+  "/crypto-monitor/precache-manifest.88b2d7c7082b3586358cd5ac91d0a7f2.js"
 );
 
 self.addEventListener('message', (event) => {
